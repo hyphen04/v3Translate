f154337893014431ea06baf7eb42f8af815a5b8e
Lazycoder26 <Rudra26_2004@outlook.com>
2023-12-24 15:01:19 +0530

    Refactor app.ts and vercel.json

--- src/app.ts
+++ src/app.ts
@@ -6,33 +6,33 @@ const fastify = Fastify({ logger: true });
 const { PAGE_COUNT = "5", PORT = "8000" } = process.env;
 
 (async () => {
-    console.log("connecting to puppeteer...");
+  console.log("connecting to puppeteer...");
 
-    console.log("connected");
+  console.log("connected");
 
-    console.log("initializing pages...");
+  console.log("initializing pages...");
 
-    try {
-        await new PagePool(parseInt(PAGE_COUNT, 10)).init();
-    } catch (e) {
-        console.log("Failed to initialize pages");
-        console.error(e);
-        process.exit(1);
-    }
+  try {
+    await new PagePool(parseInt(PAGE_COUNT, 10)).init();
+  } catch (e) {
+    console.log("Failed to initialize pages");
+    console.error(e);
+    process.exit(1);
+  }
 
-    console.log("ready");
+  console.log("ready");
 
-    // Register only the API route as the home page
-    fastify.register(require("./routers/api").default, { prefix: "/api" });
-	fastify.register(require("./routers/index").default, { prefix: "/" });
+  // Register only the API route as the home page
+  fastify.register(require("./routers/index").default, { prefix: "/" });
+  fastify.register(require("./routers/api").default, { prefix: "/api" });
 
-    try {
-        await fastify.listen({
-            port: Number(PORT),
-            host: "0.0.0.0",
-        });
-    } catch (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
+  try {
+    await fastify.listen({
+      port: Number(PORT),
+      host: "0.0.0.0",
+    });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 })();
